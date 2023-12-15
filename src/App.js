7be4acd50f1cceb02abc2674e69d1256b9beda62
daria kuznetsova <daria.kuznetsova2003@gmail.com>
2023-12-15 13:18:50 +0200

    creating header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import "./css/style.css"
+import "./css/style.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      {/* 
-      
+       
       <Header />
-      <Navigation />
+      {/*<Navigation />
       <SideBar />
       <ShopItems />
       
